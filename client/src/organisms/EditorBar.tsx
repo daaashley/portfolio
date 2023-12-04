5393a109c0f005f86b6881fc682590b373e5a6c3
Ubuntu <ubuntu@ip-172-31-3-191.ec2.internal>
2023-12-04 22:41:04 +0000

    file bar

--- client/src/organisms/EditorBar.tsx
+++ client/src/organisms/EditorBar.tsx
@@ -5,16 +5,28 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Grid } from '@mui/material';
 
 export const EditorBar = () => {
+    const files = ['test.lox', 'BinarySearch.lox','parser.lox']
     return (
         <Box sx={{ flexGrow: 1 }}>
 
             <Box sx={{ flexGrow: 1, background: 'white', height: '30px' }}>
-                <Box sx={{ flexGrow: 1, background: '#1e1e1e', height: '30px', width: '60px', display: 'flex', justifyContet: 'center', alignItems: 'center' }}>
-                    <Typography style={{ textAlign: 'center', alignSelf: 'center', color: '#cccccc' }}>test.lox</Typography>
-                </Box>
+            <Grid container >
+                {files.map((file)=> {
+                    return (
+                        <Grid item xs={0}><Box sx={{ flexGrow: 1, background: '#1e1e1e', height: '30px', width:'90px'  }}>
+                    <Typography align={'center'} style={{display:'inline-block',  color: '#cccccc' }}>test.lox</Typography>
+                    <IconButton aria-label="close" style={{color:'#cccccc', display:'inline-block'}} >
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                </Box></Grid>)
+                })}
+                </Grid>
+                
             </Box>
         </Box>
     );
